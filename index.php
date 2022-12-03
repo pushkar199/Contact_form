5eaa2058241fe3ec706fc7644d09d51340feeabd
pushkar199 <thaturpushkar99@gmail.com>
2022-12-03 19:44:41 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY CONTACT PAGE</title>
+</head>
+<body>
+    <div class="content">
+        <section>
+            <div class="register-wrapper">
+                <div class="register-block">
+                    <h3 class="register-title">CONTACT PAGE</h3>
+
+                    <form action="result.php" method="post">
+                        <input type="text" name="name" placeholder="Enter your Name"/>
+                        <input type="text" name="contact" placeholder="Enter your Contact number"/>
+                        <input type="submit" value="SUBMIT"/>
+                    </form>
+
+
+                </div>
+            </div>
+        </section>
+    </div>
+    <footer>
+        <a href="https://www.linkedin.com/in/pushkarthakur99/" target="_blank">@Pushkar</a>
+    </footer>
+    
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<style>
+.mycss{
+    text-align: center;
+	font-size: 2.0em;
+    padding-bottom: 30px;
+    margin: 0 0 20px;
+}
+</style>
+        <?php echo '<p class="mycss">'.$_POST["name"].'</p>'; ?><br>
+        <?php echo '<p class="mycss">'.$_POST["contact"].'</p>'; ?>
+   
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
